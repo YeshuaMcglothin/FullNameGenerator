6cf4cdf0c1bb9c8767b6253aaf47994753cffd19
Yeshua mcglothin <ymcglothin1@gmail.com>
2024-10-02 15:12:04 -0700

    Added Code for name
    Added code for name

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class Main {
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String Firstname;
+        String Middlename;
+        String LastName;
+        String FullName;
+        String Suffix;
+
+
+        System.out.println("Please enter your Name");
+        System.out.println("First name:");
+        Firstname = reader.nextLine();
+        System.out.println("Middle name (Initial only followed by comma):");
+        Middlename = reader.nextLine();
+        System.out.println("Last name:");
+        LastName = reader.nextLine();
+        System.out.println("Suffix:");
+        Suffix = reader.nextLine();
+
+        String Fullname = Suffix + " " + Firstname + " " + Middlename + " " + LastName;
+        System.out.println(Fullname);
+
+
+    }
+}
